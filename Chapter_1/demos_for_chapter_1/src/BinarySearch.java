baabebd48f487b6bb41d56a734a603ee269397f8
Steve Shen <349351805@qq.comgit config --global user.email 349351805@qq.com>
2017-09-25 12:53:42 +0800

    binarysearch

--- Chapter_1/demos_for_chapter_1/src/BinarySearch.java
+++ Chapter_1/demos_for_chapter_1/src/BinarySearch.java
@@ -1,3 +1,53 @@
-public class BinarySearch {
+import java.util.Arrays;
+import java.io.BufferedReader;    
+import java.io.IOException;    
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+public class BinarySearch {
+	 private BinarySearch() { 
+
+	 }
+	 public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+    public static void main(String[] args) {
+
+        // read the integers from a file
+        Scanner x=new Scanner(System.in);
+        while(x.hasNext()){
+        int m=x.nextInt();
+         //System.out.println(m);
+        int[] number=new int[m];
+        for(int i=0;i<m;i++){
+             number[i]=x.nextInt();
+        }
+        // Arrays.sort(number);
+        System.out.println(Arrays.toString(number));
+     	}
+     	
+        // int[] whitelist = ;
+
+        // // sort the array
+        // Arrays.sort(whitelist);
+
+        // // read integer key from standard input; print if not in whitelist
+        // while (!StdIn.isEmpty()) {
+        //     int key = StdIn.readInt();
+        //     if (BinarySearch.indexOf(whitelist, key) == -1)
+        //         System.out.println(key);
+        // }
+    }
 }
